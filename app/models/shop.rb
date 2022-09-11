67e621d00b8b5fa7da5a419a8e842a5404e925c7
Kei <kt10195665@yahoo.ne.jp>
2022-09-11 17:03:26 +0900

    Add: goekitの初期設定をshopモデルに追加

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,10 @@
 class Shop < ApplicationRecord
+  acts_as_mappable default_units: :kms,
+                   default_formula: :sphere,
+                   distance_field_name: :distance,
+                   lat_column_name: :latitude,
+                   lng_column_name: :longitude
+
   validates :name, presence: true, uniqueness: true
   validates :address, presence: true, uniqueness: true
   validates :latitude, presence: true

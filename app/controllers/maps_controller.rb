b2984d2c11f710bef65f535ad0f737b418a5bee6
Kei <kt10195665@yahoo.ne.jp>
2022-09-11 17:20:03 +0900

    Add: 現在位置から2km範囲のお店をデータベースから検索

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,3 +1,24 @@
 class MapsController < ApplicationController
-  def index; end
+  def index
+    search_radius = 2.0
+    default_lat = 35.64806
+    default_lng = 139.7416326
+    if params[:q]
+      latitude = geo_params[:latitude].to_f
+      longitude = geo_params[:longitude].to_f
+      shops = Shop.all.within(search_radius, origin: [latitude, longitude]).by_distance(origin: [latitude, longitude])
+      gon.latitude = latitude
+      gon.longitude = longitude
+      gon.shops = shops
+    else
+      gon.latitude = default_lat
+      gon.longitude = default_lng
+    end
+  end
+
+  private
+
+  def geo_params
+    params.require(:q).permit(:latitude, :longitude)
+  end
 end

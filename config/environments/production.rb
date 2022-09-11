13ebab70be83efcaa3021347b2034757aedcedeb
Kei <kt10195665@yahoo.ne.jp>
2022-09-11 14:40:54 +0900

    Add: gmap読み込みの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # gmap.jsの読み込み
+  config.assets.precompile += ['gmap.js']
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 

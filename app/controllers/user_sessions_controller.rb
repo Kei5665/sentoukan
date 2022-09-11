853597c0d2bb93db450ec2b52b85286e15ecacc0
Kei <kt10195665@yahoo.ne.jp>
2022-09-12 04:28:20 +0900

    Add: ゲストログイン後クエスト画面へは

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,6 +4,6 @@ class UserSessionsController < ApplicationController
     random_value = SecureRandom.hex
     user = User.create!(name: "ゲスト", email: "test_#{random_value}@example.com",)
     auto_login(user)
-    redirect_to quests_path, success: "ゲストでログインしました！クエストを開始します！"
+    redirect_to quests_path
   end
 end

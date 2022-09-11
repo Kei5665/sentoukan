91ccc5347b6cae1dab50e3a05ce556f0e428e6a3
Kei <kt10195665@yahoo.ne.jp>
2022-09-11 21:25:57 +0900

    Add: 店詳細ページの作成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,8 @@
 class ShopsController < ApplicationController
   def show
     @shop = Shop.find(params[:id])
+    longitude = @shop.longitude
+    latitude = @shop.latitude
+    @shops = Shop.all.within(2.0, origin: [latitude, longitude]).by_distance(origin: [latitude, longitude])
   end
 end

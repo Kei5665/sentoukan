c03c9a219192db600550b016a55dd1c692c3fb1c
Kei <kt10195665@yahoo.ne.jp>
2022-09-13 02:34:20 +0900

    Fix: 初期位置を調整

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,8 +1,8 @@
 class MapsController < ApplicationController
   def index
     search_radius = 2.0
-    default_lat = 35.64806
-    default_lng = 139.7416326
+    default_lat = 35.68141918996472
+    default_lng = 139.76709261065324
     if params[:q]
       latitude = geo_params[:latitude].to_f
       longitude = geo_params[:longitude].to_f

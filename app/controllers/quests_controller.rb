f5f0adfb2e95e169f3b6659b8db08e254d399081
Kei <kt10195665@yahoo.ne.jp>
2022-09-13 04:04:11 +0900

    Add: 距離を測るculculateアクション内で100範囲内の店があればクリアとする

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -13,12 +13,32 @@ class QuestsController < ApplicationController
   end
 
   def calculate
-    
+    search_radius = 0.1
+    latitude = geo_params[:latitude].to_f
+    longitude = geo_params[:longitude].to_f
+    quest = current_user.quests.last
+    @shop = quest.shop
+    gon.latitude = quest.latitude.to_f
+    gon.longitude = quest.longitude.to_f
+    gon.shop = quest.shop
+    arrival_point = Shop.within(search_radius, origin: [latitude, longitude])
+    if arrival_point.present?
+      redirect_to maps_path
+    else
+      render turbo_stream: turbo_stream.replace(
+        'error',
+        partial: 'shared/error',
+      )
+    end
   end
 
   private
 
     def quest_params
       params.require(:quest).permit(:shop_id, :user_id, :latitude, :longitude)
+    end
+
+    def geo_params
+      params.require(:q).permit(:latitude, :longitude)
     end
 end

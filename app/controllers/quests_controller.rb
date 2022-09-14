ae1f0f9e7629a48814e534fadef15c2448030ca0
Kei <kt10195665@yahoo.ne.jp>
2022-09-15 02:42:41 +0900

    Fix: 距離計算式を作成(geokitは使わない)

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,4 +1,5 @@
 class QuestsController < ApplicationController
+  require 'geokit'
   def index
     quest = current_user.quests.last
     @shop = quest.shop
@@ -17,14 +18,12 @@ class QuestsController < ApplicationController
   end
 
   def calculate
-    search_radius = 1.0
+    within_can_clear_limits = 1.25
     shop = current_user.quests.last.shop
-    latitude = geo_params[:latitude].to_f
-    longitude = geo_params[:longitude].to_f
-    arrival_point = shop.within(search_radius, origin: [latitude, longitude])
-    if arrival_point.present?
+    distance = distance(geo_params,shop)
+    if distance < within_can_clear_limits
       current_user.get_money
-      redirect_to maps_path, green: "お疲れ様でした！報酬は800円です！"
+      redirect_to maps_path, green: "クリアおめでとうございます！報酬は800円です！"
     else
       render turbo_stream: turbo_stream.prepend(
         'error',
@@ -41,5 +40,38 @@ class QuestsController < ApplicationController
 
     def geo_params
       params.require(:q).permit(:latitude, :longitude)
+    end
+
+    def distance(from,to)
+      # ラジアン単位に変換
+      x1 = deg2rad(from[:latitude])
+      y1 = deg2rad(from[:longitude])
+      x2 = deg2rad(to.latitude)
+      y2 = deg2rad(to.longitude)
+      
+      # 地球の半径 (km)
+      radius = 6378.137
+      
+      # 差の絶対値
+      diff_y = (y1 - y2).abs
+      
+      calc1 = Math.cos(x2) * Math.sin(diff_y)
+      calc2 = Math.cos(x1) * Math.sin(x2) - Math.sin(x1) * Math.cos(x2) * Math.cos(diff_y)
+      
+      # 分子
+      numerator = Math.sqrt(calc1 ** 2 + calc2 ** 2)
+      
+      # 分母
+      denominator = Math.sin(x1) * Math.sin(x2) + Math.cos(x1) * Math.cos(x2) * Math.cos(diff_y)
+      
+      # 弧度
+      degree = Math.atan2(numerator, denominator)
+      
+      # 大円距離 (km)
+      distance = degree * radius
+    end
+
+    def deg2rad(degrees)
+      degrees.to_f / 180.0 * Math::PI
     end
 end

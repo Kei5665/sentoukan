11548eb2361768d7aeb550ed0ba993193e75051f
Kei <kt10195665@yahoo.ne.jp>
2022-09-14 17:42:58 +0900

    Add: スクレイプ時保存するかどうかのカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_13_013749) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_14_084011) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,11 +31,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_13_013749) do
     t.decimal "latitude", null: false
     t.decimal "longitude", null: false
     t.string "opening_hours"
-    t.string "closing_time"
     t.string "address", null: false
     t.string "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "avalability", default: false
   end
 
   create_table "users", force: :cascade do |t|

5757d63d4884159a82183d35baced282df6d8bdf
Kei <kt10195665@yahoo.ne.jp>
2022-09-17 22:09:01 +0900

    Add: 見た目を作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get '/calculate', to: 'quests#calculate'
   post '/scrape', to: 'scrapes#scrape'
   resources :maps, only: %i[index]
-  resources :quests, only: %i[index create]
+  resources :quests, only: %i[index create show]
   resources :scrapes, only: %i[index create]
 end

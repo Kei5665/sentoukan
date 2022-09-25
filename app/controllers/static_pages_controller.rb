0e13988236321ad8978de02e6a24b3388aeb3e75
Kei <kt10195665@yahoo.ne.jp>
2022-09-26 01:33:55 +0900

    Fix: コンフリクトの修正漏れがあった

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,7 @@
 class StaticPagesController < ApplicationController
   def top; end
-<<<<<<< HEAD
-=======
 
   def terms; end
 
   def privacy_policy; end
->>>>>>> a3254c7b79e4a5c6b964ea0c318da7259824961e
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def earned_money
     "#{current_user.earned_money}円"
   end
+
+  def user_name
+    "#{current_user.name}様"
+  end
 end

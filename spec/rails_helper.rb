5c07ddaac9d0166742e3dc7c3ffbaf7ae4791d63
Kei <kt10195665@yahoo.ne.jp>
2022-09-26 18:18:44 +0900

    Fix: mapのspecを修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 require 'factory_bot'
 require 'capybara/rspec'
+require 'rspec/retry'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -40,6 +41,16 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+    # 実行中にリトライのステータスを表示する
+    config.verbose_retry = true
+    # リトライの原因となった例外を表示する
+    config.display_try_failure_messages = true
+  
+    # js: true のフィーチャスペックのみリトライを有効にする
+    config.around :each, :js do |ex|
+      ex.run_with_retry retry: 3
+    end  
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
--- spec/system/maps_spec.rb
+++ spec/system/maps_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Maps', type: :system do
   let(:near_shop) { create(:shop, name: "銀座湯", address:"住所1", latitude: 35.674653305483524, longitude: 139.77025262599443) }
   let(:far_shop) { create(:shop, name: "月島温泉", address:"住所2", latitude: 35.66375681411227, longitude: 139.78108269715878) }
-  let(:near_quest) { create(:quest, user_id: user.id, shop_id: near_shop.id, latitude: near_shop.latitude, longitude: near_shop.longitude) }
 
+  # 最初にログインする
   before do
     visit root_path
     click_on 'スタート'
@@ -16,17 +16,64 @@ RSpec.describe 'Maps', type: :system do
       visit maps_path
       expect(current_path).to eq(maps_path), 'mapsページへリダイレクトされていません'
     end
-    it "ピンが表示される" do
+
+    it "現在位置のピンが表示される" do
       within('#map') do
         expect(page.all("img[src$='https://maps.gstatic.com/mapfiles/transparent.png']").count).to eq 1
       end
     end
+
     it "ナビが表示されている" do
       expect(page).to have_selector '#nav'
     end
+
     it "メニューが表示される" do
       click_on 'nav-link'
       expect(page).to have_css '.dropdown-menu'
     end
   end
+
+  describe '検索時に表示される画面' do
+    before do
+      near_shop
+      far_shop
+      click_on '周辺を検索'
+      expect(current_path).to eq(maps_path), 'mapsページへリダイレクトされていません'
+    end
+
+    it "ピンが3つ表示される", js: true do
+      within('#map') do
+        expect(page.all("img[src$='https://maps.gstatic.com/mapfiles/transparent.png']").count).to eq 3
+      end
+    end
+
+    it "ピンをクリックすると店情報が表示される" do
+      pin = find("map#gmimap1 area", visible: false)
+      pin.click
+      expect(page).to have_content('銀座湯')
+    end
+  end
+
+  describe '検索時に表示される画面' do
+    before do
+      near_shop
+      far_shop
+      click_on '周辺を検索'
+      expect(current_path).to eq(maps_path), 'mapsページへリダイレクトされていません'
+    end
+
+    it "ピンが3つ表示される", js: true do
+      within('#map') do
+        expect(page.all("img[src$='https://maps.gstatic.com/mapfiles/transparent.png']").count).to eq 3
+      end
+    end
+
+    it "ピンをクリックすると店情報が表示される" do
+      pin = find("map#gmimap1 area", visible: false)
+      pin.click
+      expect(page).to have_content('銀座湯')
+    end
+  end
+
+
 end
